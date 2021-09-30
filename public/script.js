3682a4bd74a0c5d38e808e3f8ba26ed76cd5e110
anxious gritt <roshanjalil609@gmail.com>
2021-09-30 10:25:20 +0500

    set up tool

---
+++ public/script.js
@@ -0,0 +1,38 @@
+const videoNameInput = document.getElementById("videoName");
+const link  = document.getElementById("link");
+const result = document.getElementById("result");
+const enterBtn = document.querySelector("#enter");
+const clipBoard = document.querySelector("#copy")
+
+
+const apendToDOM = function(answer){
+    result.value = answer
+
+}
+const splitLink = function(link){
+    const newLink = link.split("&")[0];
+    console.log(newLink)
+    return newLink;
+}
+
+
+
+
+enterBtn.addEventListener("click",function(){
+    let  rawLink = link.value;
+    let  formattedLink = splitLink(rawLink);
+    let  videoName = videoNameInput.value;
+    let formattedCommand = `ffmpeg -i ${formattedLink} -c copy ${videoName}.mkv`
+
+    apendToDOM(formattedCommand)
+    link.value = ""
+    videoNameInput.value = ""
+    
+})
+
+function copyToCliboard() {
+    document.querySelector("#result").select();
+    document.execCommand('copy');
+    alert("command copied ")
+  }
+clipBoard.addEventListener("click",copyToCliboard)
